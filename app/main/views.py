100c550a6014fd9738fbeec85c6e76e7d0a2d567
ID3055 <ID3055@qq.com>
2016-01-31 10:47:41 +0800

    布局改为使用bootstrap+自定义css

--- app/main/views.py
+++ app/main/views.py
@@ -19,15 +19,13 @@ def index():
 	posts = Post.query.order_by(Post.timestamp.desc()).all()
 	return render_template('index.html',form=form,posts=posts)
 
-@main.route('/test',methods=['GET','POST'])
-def test():
-    return render_template('B.html')
 @main.route('/user/<username>')
 def user(username):
 	user = User.query.filter_by(username=username).first()
 	if user is None:
 		abort(404)
-	return render_template('user.html',user=user)
+	posts = user.posts.order_by(Post.timestamp.desc()).all()
+	return render_template('user.html',user=user,posts=posts)
 
 @main.route('/edit-profile',methods=['GET','POST'])
 @login_required
